03f6b50fecc8bea5cdcd22833c30d3878162f764
vashic <57475918+vashic@users.noreply.github.com>
2021-03-13 10:37:16 +0530

    Update api3.py

--- api-singlechild/api3.py
+++ api-singlechild/api3.py
@@ -1,24 +1,34 @@
-#run using command FLASK_APP=api3.py flask run --port 5001
-#on another terminal, run FLASK_APP=api3.py flask run --port 5002
-#run req.py on another terminal
+#FLASK_APP=api3.py flask run --port 5001
 
-
-
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
-import requests
-
+from math import exp as exp 
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self,a,b):
-               
-        return a*b
+# Calculate neuron activation for an input
+def activate(weights, inputs):
+	activation = float(weights[-1])
+	for i in range(len(weights)-1):
+		activation += float(weights[i]) * float(inputs[i])
+	return activation
+ 
+# Transfer neuron activation
+def transfer(activation):
+	return float(1.0 / float(1.0 + exp(-activation)))
+        
 
-api.add_resource(HelloWorld, '/<int:a>/<int:b>')
+@app.route('/forward')
+def forward():
+	query = request.args.to_dict(flat = False)
+	weights = query['weights']
+	inputs = query['inputs']
+	activation = activate(weights,inputs)
+	message = transfer(activation)
+
+	return str(message)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
