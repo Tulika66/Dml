77e6cdbd5c75b322b4e7d46b02b3ba9097e03571
vashic <57475918+vashic@users.noreply.github.com>
2021-05-07 12:08:05 +0530

    Update api3.py

--- async-two-child/api3.py
+++ async-two-child/api3.py
@@ -3,6 +3,9 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from math import exp as exp 
+import tracemalloc
+
+memoryuse=0
 
 app = Flask(__name__)
 api = Api(app)
@@ -23,13 +26,32 @@ def transfer(activation):
 
 @app.route('/forward')
 def forward():
+	global memoryuse
+
+	tracemalloc.start()
+
 	query = request.args.to_dict(flat = False)
 	weights = query['weights']
 	inputs = query['inputs']
+
+	#calculate memory use
+	current1, peak1 = tracemalloc.get_traced_memory()
+
 	activation = activate(weights,inputs)
 	message = transfer(activation)
 
+	current2, peak2 = tracemalloc.get_traced_memory()
+	memoryuse = max(memoryuse, ((current2-current1)/ 10**3))
+	
+	
+	tracemalloc.stop()
+
 	return str(message)
+
+@app.route('/getmemory')
+def getmemory():
+	#return the memory use to the client program
+	return str(memoryuse)
 
 
 if __name__ == '__main__':
